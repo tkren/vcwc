4b7851689cd7e2a474b41fcf3b7907cf24b470d3
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2013-09-05 14:23:32 +0200

    Typo 2.

--- lib/clone_input_dirs.sh
+++ lib/clone_input_dirs.sh
@@ -6,7 +6,8 @@ target=/home
 dirs="$source/aspcomp/{instances,benchmarks,participants,profiles}%$target/aspcomp \
 	$source/aspexec/bin%$target/aspexec \
 	$source/aspck/bin%$target/aspck \
-	$source/aspstat/{bin,profiles/t0{1..3}}%$target/aspstat"
+	$source/aspstat/bin%$target/aspstat \
+	$source/aspstat/profiles/t0{1..3}%$target/aspstat/profiles"
 
 for p in $(eval echo $dirs); do
 
