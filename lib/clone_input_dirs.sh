b6ecc5e563792028145ebbfa09900564f470a1d3
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2013-09-05 14:00:56 +0200

    Only copy track profiles.

--- lib/clone_input_dirs.sh
+++ lib/clone_input_dirs.sh
@@ -6,7 +6,7 @@ target=/home
 dirs="$source/aspcomp/{instances,benchmarks,participants,profiles}%$target/aspcomp \
 	$source/aspexec/bin%$target/aspexec \
 	$source/aspck/bin%$target/aspck \
-	$source/aspstat/{bin,profiles}%$target/aspstat"
+	$source/aspstat/{bin,profiles/t{0..3}}%$target/aspstat"
 
 for p in $(eval echo $dirs); do
 
