20c279acd0f6a7d6bc437d2df9b9180ffd2e87ca
Giovambattista Ianni <ianni@mat.unical.it>
2013-09-11 13:30:08 +0200

    -Switched the max-solv and the min-check columns -Refined tabbing

--- lib/create_table_fix_na_check.sh
+++ lib/create_table_fix_na_check.sh
@@ -4,9 +4,9 @@ set -e
 
 track=${1:?'track unset'}
 
-seq -s' ' 1 22
+seq -s "$(echo -e '\t')" 1 22
 
-echo -e "Timestamp-Statrun Timestamp-Execution Host Track-ID Benchmark-ID Solver-ID Instance-ID Stats-Discrepancy Exit-Code-Solver Exit-Code-Checker Min-Cost-Solver Max-Cost-Solver Min-Cost-Checker Max-Cost-Checker Problem-Type Wall-Clock-Time Time-Grounder Memory-Grounder Time-Solver Memory-Solver Time-Combined Memory-Combined"
+echo -e "Timestamp-Statrun Timestamp-Execution Host Track-ID Benchmark-ID Solver-ID Instance-ID Stats-Discrepancy Exit-Code-Solver Exit-Code-Checker Min-Cost-Solver Min-Cost-Checker Max-Cost-Solver Max-Cost-Checker Problem-Type Wall-Clock-Time Time-Grounder Memory-Grounder Time-Solver Memory-Solver Time-Combined Memory-Combined"
 
 find $track -mindepth 4 -maxdepth 4 -type f -name runmeans.dat | xargs tail -qn1 | sort -k5,5 -k7,7 | awk \
     'BEGIN{ bm=0; inst=0; verified=0; i=-1; first=1; }
